ca805cde7005887a82d91a2578afc5fc7999b37b
Evgenij Golub <evgenij.golub@privatbank.ua>
2021-10-15 22:42:45 +0300

    init commit

--- src/com/pb/golub/hw2/Calculator.java
+++ src/com/pb/golub/hw2/Calculator.java
@@ -30,6 +30,7 @@ public class Calculator {
                 } else {
                     System.out.println("Ваш ответ = " + (operand1 / operand2));
                 }
+
                 break;
         }
     }
