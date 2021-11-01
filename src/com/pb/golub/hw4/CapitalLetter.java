dce3d70ecc115d867a57dd62d7084da72e70fdb0
Evgenij Golub <evgenij.golub@privatbank.ua>
2021-11-01 22:00:33 +0300

    init commit

--- src/com/pb/golub/hw4/CapitalLetter.java
+++ src/com/pb/golub/hw4/CapitalLetter.java
@@ -3,18 +3,21 @@ package com.pb.golub.hw4;
 import java.util.Scanner;
 
 public class CapitalLetter {
+    static void msg(){
+        System.out.println("Введите строку");
+    }
 public static void main (String[] args){
     Scanner scan = new Scanner(System.in);
+    msg();
     String s1 = scan.next();
     String s2 = "";
-
-    s2 = s2 + s1.substring(0, 1).toUpperCase();
-    for (int i = 1; i < s1.length(); i++) {
-        if (" ".equals(s1.substring(i-1, i)))
-            s2 = s2 + s1.substring(i, i+1).toUpperCase();
-        else
-            s2 = s2 + s1.substring(i, i+1);
-        System.out.println(s2);
+    for(int i=0; i< (s1.length());i++){
+        if(s1.charAt(i)==' '){
+            s2 = s2.substring(0,i+1)+s1.substring(i+1,i+2).toUpperCase()+s1.substring(i+2);
+            }else if (i==0){
+            s2=s1.substring(0,1).toUpperCase()+s1.substring(1);
+        }
     }
-    }
+    System.out.println(s2);
+}
 }
